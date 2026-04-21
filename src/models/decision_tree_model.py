8d0eb7267d8ae77a58886723cdbfba653aaa4815
Adityavardhan Singh <realadityavardhan@gmail.com>
2026-04-21 20:07:24 +0530

    chore: add Decision Tree model script and saved model

---
+++ src/models/decision_tree_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import joblib
+import os
+
+def run_decision_tree():
+    pass
